ceb71ea744ac0497bbe866a5a82fd1b860a4e00b
djonk <djonk@DESKTOP-MNTGEEC.homenet.telecomitalia.it>
2020-05-11 10:53:51 +0200

    fine lezione(26) cammini minimi

--- src/main/java/it/polito/tdp/metroparis/model/Model.java
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -1,13 +1,23 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+
 import java.util.List;
 import java.util.Map;
 
 import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
 import org.jgrapht.Graphs;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.traverse.DepthFirstIterator;
 
 import it.polito.tdp.metroparis.db.MetroDAO;
 
@@ -69,12 +79,143 @@ public class Model {
 			this.graph.addEdge(c.getFp(), c.getFa());
 		}
 		System.out.printf("Grafo caricato con %d vertici e %d archi\n" , this.graph.vertexSet().size(), this.graph.edgeSet().size());
-		System.out.println(this.graph);
+//		System.out.println(this.graph);
+	}
+	
+	/**
+	 * visita il grafo con la strategia Breath first e ritorna l'insieme dei veritici incontrati;
+	 * @param f vertice di partenza 
+	 * @return lista dei cammini dal quel virtice di partenza
+	 */
+	public List<Fermata> visitaInAmpiezza(Fermata f) {
+		List <Fermata> visita = new ArrayList<Fermata>();
+	
+		BreadthFirstIterator<Fermata,DefaultEdge> bfv = new BreadthFirstIterator<>(graph,f);
+		
+		while(bfv.hasNext()) {
+			
+			visita.add(bfv.next());
+			
+		}
+		
+		
+		
+		return visita;
+	}
+	
+	public List<Fermata> visitaInProfondita(Fermata f) {
+		List <Fermata> visita = new ArrayList<Fermata>();
+	
+		
+		DepthFirstIterator<Fermata,DefaultEdge> dfv = new DepthFirstIterator<>(graph,f);
+		
+		while(dfv.hasNext()) {
+			
+			visita.add(dfv.next());
+			
+		}
+		
+		
+		
+		return visita;
+	}
+	
+	public Map<Fermata,Fermata> alberoVisita(Fermata source) {
+		
+		Map<Fermata,Fermata> result = new HashMap<>();
+		result.put(source, null);
+		
+		// <verticeNuovo - verticePrecedente>
+		
+		BreadthFirstIterator<Fermata,DefaultEdge> bfv = new BreadthFirstIterator<>(graph,source);
+		
+		bfv.addTraversalListener(new TraversalListener<Fermata, DefaultEdge>(){
+
+			@Override
+			public void connectedComponentFinished(ConnectedComponentTraversalEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void connectedComponentStarted(ConnectedComponentTraversalEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void edgeTraversed(EdgeTraversalEvent<DefaultEdge> e) {
+				//la visita sta considerando un arco 
+				//questo arco ha scoperto un nuovo vertice?
+				//SE SI da dove provenivo?
+				DefaultEdge g = e.getEdge();
+				Fermata a = graph.getEdgeSource(g);
+				Fermata b = graph.getEdgeTarget(g);
+				if(result.containsKey(a)) {
+					result.put(b, a);
+				}else {
+					result.put(a, b);
+				}
+			}
+
+			@Override
+			public void vertexTraversed(VertexTraversalEvent<Fermata> e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void vertexFinished(VertexTraversalEvent<Fermata> e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		while(bfv.hasNext()) {
+			bfv.next();
+		}
+		
+		return result;
+	}
+	
+	public List<Fermata> camminiMinimi(Fermata partenza, Fermata arrivo) {
+		
+		DijkstraShortestPath<Fermata,DefaultEdge>  dij = new DijkstraShortestPath<>(graph);
+		
+		GraphPath<Fermata,DefaultEdge> cammino =  dij.getPath(partenza, arrivo);
+		
+		
+		return cammino.getVertexList();
+		
 	}
 	
 	public static void main (String args[]) {
 		
 		Model m = new Model();
+		
+//		List<Fermata> visita = m.visitaInAmpiezza(m.fermate.get(0));
+//		
+//		System.out.println("\\\\\\\\\\\\\\\\\\");
+//		System.out.println(visita);
+//		
+//		List<Fermata> vis = m.visitaInProfondita(m.fermate.get(0));
+//		
+//		System.out.println("\\\\\\\\\\\\\\\\\\");
+//		System.out.println(vis);
+//		
+//		Map <Fermata,Fermata> a = m.alberoVisita(m.fermate.get(0));
+//		System.out.println("\\\\\\\\\\\\\\\\\\");
+//		
+//		for(Fermata f : a.keySet()) {
+//			System.out.format("%s -> %s \n", f, a.get(f));
+//		}
+		
+		List <Fermata> cammino = m.camminiMinimi(m.fermate.get(0), m.fermate.get(1));
+		
+		System.out.println(cammino);
+		
+				
 		
 	}
 	
